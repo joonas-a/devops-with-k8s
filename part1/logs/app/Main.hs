43615ec2f143fdb46b5998b923f68dcff40ab959
joonas-a <joonas.ahovalli@gmail.com>
2024-11-03 13:15:26 +0200

    1.07

--- part1/logs/app/Main.hs
+++ part1/logs/app/Main.hs
@@ -1,6 +1,14 @@
 module Main (main) where
 
 import Lib
+import Control.Concurrent (forkIO)
+import qualified Data.UUID.V4 as UUIDv4
+import Data.UUID (toString)
+
 
 main :: IO ()
-main = stringGen
+main = do
+  uuid <- UUIDv4.nextRandom
+  let uuidStr = toString uuid
+  _ <- forkIO (stringGen uuidStr)
+  endpoint uuidStr
--- part1/logs/src/Lib.hs
+++ part1/logs/src/Lib.hs
@@ -1,24 +1,36 @@
 module Lib
-    ( stringGen
+    ( stringGen, endpoint
     ) where
 
 import System.IO (stdout, hFlush)
 import Control.Monad (forever)
 import Control.Concurrent (threadDelay)
-import Data.UUID (toString)
 import Data.Time.Clock (getCurrentTime, diffUTCTime)
 import Data.Time.Format.ISO8601 (formatShow, iso8601Format)
-import qualified Data.UUID.V4 as UUIDv4
+import Network.HTTP.Types.Status (status200)
+import Network.Wai (Application, responseLBS)
+import Network.Wai.Handler.Warp (run)
+import qualified Data.ByteString.Lazy.Char8 as BL
 
-stringGen :: IO ()
-stringGen =
-  UUIDv4.nextRandom >>= (\s ->
+stringGen :: String -> IO ()
+stringGen uuid =
     forever $ do
       start <- getCurrentTime
-      print $ formatShow iso8601Format start <> ": " <> toString s
+      print $ formatShow iso8601Format start <> ": " <> uuid
       hFlush stdout
       end <- getCurrentTime
       let elapsed = realToFrac (diffUTCTime end start) :: Double
       let delay = max 0 (5 - elapsed)
       threadDelay (round (delay * 1000000))
-  )
+
+port :: Int
+port = 2341
+
+app :: String -> Application
+app uuid _req res = do
+  time <- getCurrentTime
+  let message = formatShow iso8601Format time <> ": " <> uuid
+  res (responseLBS status200 [] (BL.pack message))
+
+endpoint :: String -> IO ()
+endpoint uuid = run port (app uuid)
