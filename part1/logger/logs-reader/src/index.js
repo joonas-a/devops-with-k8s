72e499e54b13c18694463dc97c0b9f95a213f91b
joonas-a <joonas.ahovalli@gmail.com>
2024-12-18 17:00:11 +0200

    1.12

--- part1/logger/logs-reader/src/index.js
+++ part1/logger/logs-reader/src/index.js
@@ -1,23 +1,74 @@
 const fs = require('fs').promises;
+const { createWriteStream } = require('fs');
 const Koa = require('koa');
 const path = require('path');
+const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 
 const app = new Koa();
 
 const port = process.env.PORT || 2341;
 
+// Path for local testing
+// const dir = path.join(__dirname, 'files');
 const dir = path.join('/', 'usr', 'src', 'app', 'files');
+
 const filepath = path.join(dir, 'logs.txt');
 const pongpath = path.join(dir, 'pong.txt');
+const imagepath = path.join(dir, 'image.jpg');
+const imageURL = 'https://picsum.photos/400';
 
 const hash = uuidv4();
 
+const imageFetcher = async () => {
+  try {
+    const response = await axios.get(imageURL, { responseType: 'stream' });
+    const writer = createWriteStream(imagepath);
+    response.data.pipe(writer);
+    await new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+    console.log('Fetched image.');
+  } catch (err) {
+    console.error('Failed to fetch image from remote.', err);
+  }
+};
+
+const imageOlderThanHour = async () => {
+  try {
+    const { birthtime } = await fs.stat(imagepath);
+    const now = new Date();
+    const diff = now - birthtime;
+    const diffHours = diff / 1000 / 60 / 60;
+    return diffHours > 1;
+  } catch (err) {
+    console.error('Failed to check image age.', err);
+    return true;
+  }
+};
+
 app.use(async (ctx) => {
   try {
+    const shouldRenew = await imageOlderThanHour();
+    shouldRenew && (await imageFetcher());
     const time = await fs.readFile(filepath, 'utf8');
     const pongs = await fs.readFile(pongpath, 'utf8');
-    ctx.body = `${time} ${hash} \nPing / Pongs: ${pongs}`;
+    const base64Image = await fs.readFile(imagepath, 'base64');
+    ctx.type = 'html';
+    ctx.body = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Logger App</title>
+      </head>
+      <body>
+        <p>${time} ${hash}</p>
+        <p>Ping / Pongs: ${pongs}</p>
+        <img src="data:image/jpeg;base64,${base64Image}">
+      </body>
+      </html>
+    `;
   } catch (err) {
     console.error('FAILED TO READ FILE', err);
     ctx.status = 500;
