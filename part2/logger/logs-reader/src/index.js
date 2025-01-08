511b9032fa5bcd09b09a86cef16e891100bcc6b8
joonas-a <joonas.ahovalli@gmail.com>
2025-01-08 14:04:40 +0200

    2.06

--- part2/logger/logs-reader/src/index.js
+++ part2/logger/logs-reader/src/index.js
@@ -7,18 +7,22 @@ const { v4: uuidv4 } = require('uuid');
 const app = new Koa();
 
 const port = process.env.PORT || 2341;
+const envMessage = process.env.MESSAGE || 'N/A';
 
 // Path for local testing (remember to uncomment)
 //const dir = path.join(__dirname, 'files');
 const dir = path.join('/', 'usr', 'src', 'app', 'files');
+const config = path.join('/', 'usr', 'src', 'app', 'config');
 
 const filepath = path.join(dir, 'logs.txt');
+const configpath = path.join(config, 'information.txt');
 
 const hash = uuidv4();
 
 app.use(async (ctx) => {
   try {
     const time = await fs.readFile(filepath, 'utf8');
+    const information = await fs.readFile(configpath, 'utf8');
     const pongs = await axios
       .get('http://pong-svc:2350/api')
       .then((res) => res.data)
@@ -34,6 +38,8 @@ app.use(async (ctx) => {
         <title>Logger App</title>
       </head>
       <body>
+        <p>file content: ${information}</p>
+        <p>env variable: ${envMessage}</p>
         <p>${time} ${hash}</p>
         <p>Ping / Pongs: ${pongs}</p>
       </body>
